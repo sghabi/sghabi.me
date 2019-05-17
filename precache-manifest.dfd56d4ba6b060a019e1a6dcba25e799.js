ebe35ce9d430963ec874fec26abae4ed49e4b58d
Sonia Ghabi <sonia.ghabi@gmail.com>
2019-05-17 22:39:18 +0800

    Updates

---
+++ precache-manifest.dfd56d4ba6b060a019e1a6dcba25e799.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf798280a2877475c5003452cb0476f",
+    "url": "/sghabi.me/index.html"
+  },
+  {
+    "revision": "7066bac5be6b4e39955b",
+    "url": "/sghabi.me/static/css/main.e80c587a.chunk.css"
+  },
+  {
+    "revision": "51a177444f57b9858cb5",
+    "url": "/sghabi.me/static/js/2.9126c384.chunk.js"
+  },
+  {
+    "revision": "7066bac5be6b4e39955b",
+    "url": "/sghabi.me/static/js/main.1aac7520.chunk.js"
+  },
+  {
+    "revision": "f0b585f50a4ac15a2700",
+    "url": "/sghabi.me/static/js/runtime~main.4b6d16d8.js"
+  },
+  {
+    "revision": "ec3b8eac7e5f946d6a5d5114a0cf4f21",
+    "url": "/sghabi.me/static/media/wallpaper.ec3b8eac.jpg"
+  }
+]);
